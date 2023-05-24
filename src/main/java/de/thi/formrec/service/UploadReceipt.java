c44e9b7a94316126c76adca989b285cfb59c287d
Philipp Phan <philippphan@gmail.com>
2023-05-24 10:22:59 +0200

    :construction: Updated UploadReceipt.java and pom.xml

--- src/main/java/de/thi/formrec/service/UploadReceipt.java
+++ src/main/java/de/thi/formrec/service/UploadReceipt.java
@@ -3,16 +3,13 @@ package de.thi.formrec.service;
 import de.benevolo.entities.finance.FinancialTransaction;
 import de.thi.formrec.FormRecognizer;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.Set;
 
-@Path("/formrec")
+
+@ApplicationScoped
 public class UploadReceipt {
 
     @Inject
@@ -21,10 +18,7 @@ public class UploadReceipt {
     @Inject
     ReceiptTransform receiptTransform;
 
-    @Path("/upload")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.TEXT_PLAIN)
+
     public Set<FinancialTransaction> uploadReceipt(String url) throws IOException {
         return receiptTransform.transform(formRecognizer.recognize(url)).getFinancialTransactions();
     }
