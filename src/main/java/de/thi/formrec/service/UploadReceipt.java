a6220d5a6f628940d51ae05779796b23cf0d5ff4
Philipp <philippphan@gmail.com>
2023-05-25 21:29:14 +0200

    :wrench: restored REST Endpoint

--- src/main/java/de/thi/formrec/service/UploadReceipt.java
+++ src/main/java/de/thi/formrec/service/UploadReceipt.java
@@ -3,13 +3,17 @@ package de.thi.formrec.service;
 import de.benevolo.entities.finance.FinancialTransaction;
 import de.thi.formrec.FormRecognizer;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.Set;
 
 
-@ApplicationScoped
+@Path("/formrec")
 public class UploadReceipt {
 
     @Inject
@@ -19,6 +23,10 @@ public class UploadReceipt {
     ReceiptTransform receiptTransform;
 
 
+    @Path("/upload")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
     public Set<FinancialTransaction> uploadReceipt(String url) throws IOException {
         return receiptTransform.transform(formRecognizer.recognize(url));
     }
