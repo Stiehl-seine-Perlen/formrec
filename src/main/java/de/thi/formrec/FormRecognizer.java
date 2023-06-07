e7e1f7dc5e8b49ef8bfd24849d0fb1a39735453f
Jan Vorhoff <jan.vorhoff@gmx.de>
2023-06-07 09:56:54 +0200

    ⚰️ Remove unneeded imports

--- src/main/java/de/thi/formrec/FormRecognizer.java
+++ src/main/java/de/thi/formrec/FormRecognizer.java
@@ -6,9 +6,6 @@ import com.azure.ai.formrecognizer.documentanalysis.models.AnalyzeResult;
 import com.azure.ai.formrecognizer.documentanalysis.models.OperationResult;
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.util.polling.SyncPoller;
-import com.fasterxml.jackson.databind.DeserializationConfig;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.thi.formrec.model.Receipt;
 import org.apache.commons.lang3.NotImplementedException;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -20,8 +17,7 @@ import javax.inject.Inject;
 import java.io.IOException;
 
 @ApplicationScoped
-public class FormRecognizer
-{
+public class FormRecognizer {
 	private static final Logger LOG = LoggerFactory.getLogger(FormRecognizer.class);
 
 	@ConfigProperty(name = "de.thi.azure.endpoint")
@@ -33,21 +29,19 @@ public class FormRecognizer
 	@Inject
 	ReceiptFactory receiptFactory;
 
-	public Receipt recognize(String documentUrl) throws IOException
-	{
+	public Receipt recognize(String documentUrl) throws IOException {
 		DocumentAnalysisClient client = new DocumentAnalysisClientBuilder()
-			.credential(new AzureKeyCredential(key))
-			.endpoint(endpoint)
-			.buildClient();
+				.credential(new AzureKeyCredential(key))
+				.endpoint(endpoint)
+				.buildClient();
 
 		String modelId = "prebuilt-receipt";
-		SyncPoller<OperationResult, AnalyzeResult> analyzeDocumentPoller =
-			client.beginAnalyzeDocumentFromUrl(modelId, documentUrl);
+		SyncPoller<OperationResult, AnalyzeResult> analyzeDocumentPoller = client.beginAnalyzeDocumentFromUrl(modelId,
+				documentUrl);
 
 		AnalyzeResult analyzeResult = analyzeDocumentPoller.getFinalResult();
 
-		if (analyzeResult.getDocuments().size() > 1)
-		{
+		if (analyzeResult.getDocuments().size() > 1) {
 			throw new NotImplementedException("Only one page receipts are supported for now");
 		}
 
